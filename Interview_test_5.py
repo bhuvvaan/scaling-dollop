f09822f88e63c4fe1ee362392558f7d8edd62350
bhuvvaan <85916470+bhuvvaan@users.noreply.github.com>
2021-06-25 20:46:59 +0530

    Update Interview_test_5.py

--- Interview_test_5.py
+++ Interview_test_5.py
@@ -1,47 +1,52 @@
-bracket_string = input().split()
+input_brackets_string = input().split()
 left_brackets = {'(':1,'{':2,'[':3}
 right_brackets = {')':1,'}':2,']':3}
-m = 0
-while m < len(bracket_string):
-    item = bracket_string[m]
-    i = 0
-    outer_loop_breaker = False
-    left = []
-    count = 0
-    while i < len(item):
-        if i == 0 and item[0] in right_brackets.keys():
+i = 0
+while i<len(input_brackets_string):
+    item = input_brackets_string[i]
+    if len(item)%2!=0:
+            print('NO')
+            i+=1
+            continue
+    j=0
+    break_all_loops = False
+    while j<len(item):
+        if item[j] in right_brackets.keys():
             print('NO')
             break
-        if len(item)%2 != 0:
-            print('NO')
-            break
-        if item[i] in left_brackets.keys():
-            left.append(item[i])
-            count += 1 
-            try:
-                if item[i+1] in right_brackets.keys():
-                    if right_brackets[item[i+1]] == left_brackets[item[i]]:
-                        for k in range(count):
-                            if right_brackets[item[i-k+count]] != left_brackets[left[k]]:    
-                                print('NO')
-                                outer_loop_breaker = True
-                                break
-                        else:
-                            i = i+count
-                            left = []
-                            count = 0
-                        if outer_loop_breaker:
-                            break
+        if item[j] in left_brackets.keys():
+            k=1
+            while j<len(item):
+                j+=1
+                try:
+                    if item[j] in left_brackets.keys():
+                        k+=1
+                        continue
                     else:
-                        print('NO')
-                        break
-                else:
-                    i += 1
-                    continue
-            except IndexError:
-                print('NO')
+                        l=k-1
+                        w=j
+                        while l>=0:
+                            try:
+                                if right_brackets[item[j]] == left_brackets[item[w-k+l]]:   
+                                    l-=1
+                                    j+=1
+                                    continue
+                                else:
+                                    print('NO')
+                                    break_all_loops = True
+                                    break
+                            except:
+                                print('NO')
+                                break_all_loops = True
+                                break
+                        if break_all_loops:
+                            break
+                except:
+                    print('NO')
+                    break_all_loops = True
+                    break
+            if break_all_loops:
                 break
-        i += 1
     else:
         print('YES')
-    m += 1
+    i+=1    
